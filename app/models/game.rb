10bd1ea7abb81f0f7302388e007b7494f6f860f8
Laurie Boyes <laurie.boyes@madetech.com>
2021-11-17 09:42:12 +0000

    Some pseudocode

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,6 +6,15 @@ class Game < ApplicationRecord
   def init
     self.roundsFinished = 0
 
-    deck = Services::Deck.get_fresh_deck
+    # get deck  deck = Services::Deck.get_fresh_deck
+    # market = 3 camels + 2 from deck
+    # player1 = new player
+    #   roundsWon = 0
+    #   hand = 5 from deck
+    #   headCount = take any from hand
+    #   tokens = []
+    # game.tokens = the start tokens
+
+    # save it
   end
 end
--- spec/lib/services/deck_spec.rb
+++ spec/lib/services/deck_spec.rb
@@ -3,7 +3,12 @@
 require 'services/deck'
 
 describe 'Services::Deck' do
-  it 'creates a new deck' do
+  it 'returns an array of card names representing all of the cards in the game, except for the three start camels' do
     expect(Services::Deck.get_fresh_deck).to eql('lol')
+    # get the deck, sort it, and check it’s the same
+  end
+
+  it 'returns the array shuffled' do
+    # get the deck and assert that it’s not in the same order
   end
 end
